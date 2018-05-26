eeaca2b2918a9dce3bef1cd7a0f5e6a727c2bbd8
John Sparger <jsparger87@gmail.com>
2018-05-26 18:56:16 +0200

    first draft. seems to work. see demo in test/visualization.html

---
+++ js/gravis.js
@@ -0,0 +1,141 @@
+define(["d3"], (d3) => {
+
+  // a valid entity in the graph must not be null and must have an id.
+  function is_valid_entity(x) {
+    return (x && x.hasOwnProperty("id"));
+  }
+
+  function is_node(x) {
+    return is_valid_entity(x) && !(x.hasOwnProperty("source") || x.hasOwnProperty("target"));
+  }
+
+  function is_relationship(x) {
+    return is_valid_entity(x) && is_node(x.source) && is_node(x.target);
+  }
+
+  class Graph {
+    constructor() {
+      this._nodes = {};
+      this._links = {};
+    }
+
+    add(x) {
+      if (is_node(x)) {
+        this._nodes[x.id] = x;
+      }
+      else if (is_relationship(x)) {
+        this.add(x.source);
+        this.add(x.target);
+        this._links[x.id] = x;
+      }
+    }
+
+    remove(x) {
+      if (is_node(x)) {
+        delete this._nodes[x.id];
+      }
+      else if (is_relationship(x)) {
+        delete this._links[x.id];
+      }
+    }
+  };
+
+  function get_id(x) {
+    return x.id;
+  }
+
+  class Vis {
+    constructor(graph, width, height) {
+      this._graph = graph;
+      this._width = width;
+      this._height = height;
+      this._sim = d3.forceSimulation()
+        .velocityDecay(0.1)
+        .force("charge", d3.forceManyBody().strength(-3))
+        .force("link", d3.forceLink().distance(50).strength(1))
+        // .force("center", d3.forceCenter(this._width/2, this._height/2))
+        .on("tick", this.tick.bind(this));
+    }
+
+    init() {
+      // create svg
+      this._svg = d3.select("#force").append("svg")
+        .attr("id", "forcesvg")
+        .attr("preserveAspectRatio", "none")
+        .attr("viewBox", `0 0 ${this._width} ${this._height}`)
+        .classed("svg-content", true);
+        // .on('mousedown', this.mousedown.bind(this));
+
+      // create groups in the svg for links and nodes
+      // do links first so links will be drawn underneath nodes.
+      this._links = this._svg.append("g").selectAll(".link");
+      this._nodes = this._svg.append("g").selectAll(".node");
+    }
+
+    update() {
+      this._update_entities();
+      this.restart_simulation();
+    }
+
+    _update_entities() {
+      this._nodes = this._update_selection(this._nodes, Object.values(this._graph._nodes), this._process_node_enter);
+      this._links = this._update_selection(this._links, Object.values(this._graph._links), this._process_link_enter);
+    }
+
+    _update_selection(s, x, onEnter) {
+      s = s.data(x, get_id);
+      s.exit().remove();
+      return onEnter(s.enter()).merge(s);
+      // return s.enter().merge(s);
+    }
+
+    _process_node_enter(s) {
+      let group = s.append("g").attr("class", "node");
+      group.append("circle").attr("r", 5);
+      return group;
+    }
+
+    _process_link_enter(s) {
+      let group = s.append("g").attr("class","link");
+      group.append("line");
+      return group;
+    }
+
+    restart_simulation() {
+      this._sim.nodes(Object.values(this._graph._nodes));
+      this._sim.force("link").links(Object.values(this._graph._links));
+      this._sim.alpha(1).restart();
+    }
+
+    tick() {
+      // update the positions of the nodes in the svg based on their coordinates
+      // as computed by the force layout.
+      let bound = (x, limit, pad) => {
+        x = (x < pad) ? pad : x;
+        x = (x > limit-pad) ? limit-pad : x;
+        return x;
+      };
+
+      let pad = 10
+      this._nodes.attr("transform", function (d) {
+        d.x = bound(d.x, this._width, pad);
+        d.y = bound(d.y, this._height, pad);
+        return "translate(" + d.x + "," + d.y + ")";
+      });
+
+      // update the endpoints of the lines based on their source and end node
+      // locations.
+      this._links.selectAll("line")
+        .attr("x1", function(d) { return this._graph._nodes[d.source.id].x; }.bind(this))
+        .attr("y1", function(d) { return this._graph._nodes[d.source.id].y; }.bind(this))
+        .attr("x2", function(d) { return this._graph._nodes[d.target.id].x; }.bind(this))
+        .attr("y2", function(d) { return this._graph._nodes[d.target.id].y; }.bind(this));
+    }
+  }
+
+  return {
+    "Graph": Graph,
+    "Vis": Vis,
+  };
+
+});
